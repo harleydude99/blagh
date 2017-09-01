179e725ada7fc383e5625410b0d2513220993e60
Harley Dude <harleydude99@gmail.com>
2017-09-01 15:18:44 -0700

    add pubdate, other tweaks

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  generatePubDate(){
+    let d = new Date(),
+    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'pm' : 'am',
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+    return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
+  },
+  genPubDate(){
+    var date = new Date();
+    var n = date.toDateString();
+    var time = date.toLocaleTimeString();
+    return n + ' ' + time;
+  },
   actions: {
     saveNewPost(){
+      let publishDate = this.genPubDate();
       this.store.createRecord('post', {
         title: this.get('title'),
-        body: this.get('body')
+        author: this.get('author'),
+        body: this.get('body'),
+        pubDateTime: publishDate
       }).save();
       this.set('title', '');
+      this.set('author', '');
       this.set('body', '');
     },
     deletePost(post){
